9fe900c3adfe41a298ee4f47e1d656ce0e209e7b
Nicky <ng519@live.mdx.ac.uk>
2021-01-26 13:11:44 +0100

    Added CRUD basics

--- server.js
+++ server.js
@@ -1,7 +1,45 @@
 var express = require("express");
 var app = express();
 
+app.use(express.json());
 
+const MongoClient = require('mongodb').MongoClient;
+let db;
+
+MongoClient.connect("mongodb+srv://testboy:testboy@cluster0.pkzla.mongodb.net/vue2assignment?retryWrites=true&w=majority"
+, (err, client) => {
+    db = client.db('vue2assignment')
+})
+
+// dispaly a message for root path to show that API is working
+app.get('/'
+, function (req, res) {
+    res.send('Select a collection, e.g., /collection/messages')
+})
+
+// get the collection name
+app.param('collectionName', (req, res, next, collectionName) => {
+    req.collection = db.collection(collectionName)
+
+    // console.log('collection name:', req.collection)
+    return next()
+})
+
+// retrieve info from DB
+app.get('/collection/:collectionName', (req, res) => {
+    req.collection.find({}).toArray((e, results) => {
+    if (e) return next(e)
+    res.send(results)
+    })
+})
+
+// insert info into DB
+app.post('/collection/:collectionName', (req, res, next) => {
+    req.collection.insert(req.body, (e, results) => {
+        if (e) return next(e)
+        res.send(results.ops)
+    })
+})
 
 app.listen(3000, function() {
     console.log("Hi");
