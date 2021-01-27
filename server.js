911fa06922aeb0dbb183386212e4443ecd2486d3
Nicky <ng519@live.mdx.ac.uk>
2021-01-27 15:06:54 +0100

    Maybe update

--- server.js
+++ server.js
@@ -67,14 +67,14 @@ app.post('/collection/:collectionName', (req, res, next) => {
 // Update space avail.
 app.put('/collection/:collectionName/:id', (req, res, next) => {
     req.collection.update(
-        { _id: new ObjectID(req.params.id) }, 
+        { _id: new ObjectID(req.params.id) },
         { $set: req.body },
         { safe: true, multi: false },
         (e, result) => {
             if (e) return next(e)
-            res.send((result.result.n === 1) ?
-            {msg: 'success'} : { msg: 'error'})
-        })
+                res.send((result.result.n === 1) ?
+                {msg: 'success'} : { msg: 'error'})
+    })
 })
 
 app.listen(port, function() {
